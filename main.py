e092f261302cb606a93dd942ce35965e55421d68
abdo <mr.spykhattab@gmail.com>
2023-04-29 20:56:47 +0300

    Added Maze map

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-
+from maze import *
+from car import *
 
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 700
@@ -18,17 +19,8 @@ def init_proj():
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
-    glLineWidth(1)
-    glColor3f(1, 0, 0)
-    glBegin(GL_LINES)
-    for i in range(8):
-        glVertex(-1500, i*100, 0)
-        glVertex(1200, i*100, 0)
-    for i in range(9):
-        glVertex(i*150, -1500, 0)
-        glVertex(i*150, 1500, 0)
-    glEnd()
-    glFlush()
+    draw_map()
+    glutSwapBuffers()
 
 
 def Timer(v):
